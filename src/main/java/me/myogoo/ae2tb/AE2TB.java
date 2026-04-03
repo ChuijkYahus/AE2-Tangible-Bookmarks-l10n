601c2a140698a3f512536e5da34717f5d8068af3
njoyshadow <shs3740413@gmail.com>
2026-04-03 22:15:51 +0900

    fix: client load crash

--- src/main/java/me/myogoo/ae2tb/AE2TB.java
+++ src/main/java/me/myogoo/ae2tb/AE2TB.java
@@ -1,13 +1,14 @@
 package me.myogoo.ae2tb;
 
+import me.myogoo.ae2tb.client.AE2TBClient;
 import me.myogoo.ae2tb.init.AE2TBConfig;
-import me.myogoo.ae2tb.init.AE2TBConfigTab;
 import me.myogoo.ae2tb.init.AE2TBItems;
 import me.myogoo.ae2tb.init.AE2TBNetwork;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(AE2TB.MODID)
 public class AE2TB {
@@ -17,7 +18,6 @@ public class AE2TB {
         AE2TBConfig.initialize(modContainer);
         AE2TBItems.REGISTER.register(modEventBus);
         modEventBus.addListener(AE2TBNetwork::init);
-        AE2TBConfigTab.initialize();
     }
 
     public static ResourceLocation makeId(String path) {
